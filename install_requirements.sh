1283fcb59495e2ec315db4c10f31c855b073b4d9
AlxcNL <J.A.Boogaard@hr.nl>
2025-09-22 14:38:55 +0200

    Refactoring

---
+++ install_requirements.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+conda install -y \
+    anyio \
+    attrs \
+    certifi \
+    click \
+    colorama \
+    dnspython \
+    exceptiongroup \
+    fastapi \
+    h11 \
+    httpcore \
+    httpx \
+    idna \
+    iniconfig \
+    packaging \
+    pluggy \
+    pydantic \
+    pymongo \
+    pytest \
+    python-dateutil \
+    rfc3986 \
+    six \
+    sniffio \
+    starlette \
+    tomli \
+    typing_extensions \
+    uvicorn
